1b316b225cae40e05b8ad064377ed52126e1b4ad
WumaCoder <1829913225@qq.com>
2021-07-20 10:53:24 +0800

    chore: base ts dev

--- packages/uploader/lib/uploader.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = uploader;
-
-function uploader() {
-    // TODO
-}
---
+++ packages/uploader/rollup.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+
+export default defineConfig({
+  input: "./src/index.ts",
+  output: [
+    {
+      file: "./lib/uploader.cjs.js",
+      format: "umd",
+    },
+    {
+      file: "./lib/uploader.esm.js",
+      format: "esm",
+    },
+  ],
+  plugins: [typescript()],
+});
